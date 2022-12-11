b505953f2c9b39e8be3ad6b8c6da87085cedcc4a
Alberto Ramos Suárez <bertoramos2001@gmail.com>
2022-12-11 13:59:23 +0100

    Face Detector App finished

--- Face Recognition/Face_Detector.py
+++ Face Recognition/Face_Detector.py
@@ -3,20 +3,30 @@ import cv2
 # load some pre-trained data on face frontals from opencv
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-#choose an image to detect faces in
-img = cv2.imread('elon.jpeg')
+#to capture video from webcam
+webcam = cv2.VideoCapture(0) # (0) means it uses the default system camera
 
-#convert the image to grayscale
-grayScaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#iterate over the frames
+while True:
+    successful_frame_read, frame = webcam.read()
 
-#Detect faces
-face_coordinates = trained_face_data.detectMultiScale(grayScaled_img)
+    #convert the frame to grayscale
+    grayScaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-#Draw rectangles around the faces
-(x, y, w, h) = face_coordinates[0] #we get x,y coordinate of top left and then width and height
-cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2) #position top left, add width, add height, color green and thickness 2
+    #Detect faces
+    face_coordinates = trained_face_data.detectMultiScale(grayScaled_frame)
 
-cv2.imshow('Alberto Ramos Face Detector', img)
-cv2.waitKey()
+    #Draw rectangles around the faces
+    for (x, y, w, h) in face_coordinates: #we get x,y coordinate of top left and then width and height on each face we detect
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) #position top left, add width, add height, color green and thickness 2
+
+    #Display the image with faces detected
+    cv2.imshow('Alberto Ramos Face Detector', frame)
+    key = cv2.waitKey(1)
+
+    #stop is q is pressed
+    if key == 81 or key == 113:
+        break
+
 
 print("Code Completed")
