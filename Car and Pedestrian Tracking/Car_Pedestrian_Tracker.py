93c5cb062a527a719aa4b927ae539524a82cbfd9
Alberto Ramos Suárez <bertoramos2001@gmail.com>
2022-12-14 17:18:14 +0100

    Car and Pedestrian Tracker finished

--- Car and Pedestrian Tracking/Car_Pedestrian_Tracker.py
+++ Car and Pedestrian Tracking/Car_Pedestrian_Tracker.py
@@ -4,10 +4,12 @@ import cv2
 video = cv2.VideoCapture('carVideo.mp4')
 
 #Pre-trained data on cars from opencv
-classifier_file = 'car_detector.xml'
+car_classifier_file = 'car_detector.xml'
+pedestrian_classifier_file = 'haarcascade_fullbody.xml'
 
 # load some pre-trained data on cars from opencv
-trained_car_data = cv2.CascadeClassifier(classifier_file)
+trained_car_data = cv2.CascadeClassifier(car_classifier_file)
+trained_pedestrian_data = cv2.CascadeClassifier(pedestrian_classifier_file)
 
 while True:
 
@@ -22,13 +24,22 @@ while True:
 
         #detect cars in every frame of the video
         cars = trained_car_data.detectMultiScale(grayscaled_frame) 
+        pedestrians = trained_pedestrian_data.detectMultiScale(grayscaled_frame)
 
+        #cars will be in red squares
         for (x, y, w, h) in cars:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        #pedestrians will be in green squares
+        for (x, y, w, h) in pedestrians:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
         cv2.imshow('Alberto Ramos Car Detector', frame)
 
-        cv2.waitKey(1)
+        key = cv2.waitKey(1)
+
+        #Stop if Q is pressed
+        if key == 81 or key == 113:
+            break
 
 # Close the video
 video.release()
